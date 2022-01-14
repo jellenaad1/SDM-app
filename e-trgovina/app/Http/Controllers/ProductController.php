b8352a40b42c6cd67211e972e526e7b5cbace58d
jellenaad1 <jellenaad1@gmail.com>
2022-01-14 23:39:02 +0100

    Dodata funkcija za dodavanje proizvoda u korpu

--- e-trgovina/app/Http/Controllers/ProductController.php
+++ e-trgovina/app/Http/Controllers/ProductController.php
@@ -3,11 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Cart;
+
+
+use Illuminate\Support\Facades\Session;
+
+
+
+
 
 class ProductController extends Controller
 {
     //
     function index(){
-        return "Dobrodosli";
+        //return Product::all();
+ //ovo vraca podatke u json faju
+
+        $data = Product::all();
+        return view('product',['products'=>$data]);
     }
+
+    function detail($id){
+        $data= Product::find($id);
+        return view('detail',['product'=>$data]);
+
+    }
+
+    /*function search(Request $req){
+        $data = Product::where('name', 'like', '%'.$req->input('query').'%')->get();
+        return view('search',['product'=>$data]);
+    }*/
+
+
+    function addToCart(Request $req){
+
+        if($req->session()->has('user')){
+
+            $cart = new Cart;
+            $cart->user_id=$req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
+
+
+        }
+        else{
+            return redirect('/login');
+        }
+        
+    }
+    
+
+    static function cartItem(){
+        $userId= Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count();
+
+
+    }
+
+
+
 }
